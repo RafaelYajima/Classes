ae4c1359dbd80d4ef0236ded248d7d2966f22a57
Rafaelyajima <rafaelyajima2@gmail.com>
2023-10-28 15:43:15 -0300

    açoões da girafa

---
+++ main.py
@@ -0,0 +1,7 @@
+from girafa import Girafa
+
+girafa = Girafa("Melmal", 4.15, "Amarela e Marrom", 32, "Africa Central")
+girafa2 = Girafa("Gepeto", 5.15, "Amarela e Verde", 29, "Africa Central")
+
+print(girafa.nome)
+(girafa2.__fome)
