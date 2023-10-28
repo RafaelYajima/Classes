38aac57bd2d1a5f68e8a08fcfa6e97472d8a7151
Rafaelyajima <rafaelyajima2@gmail.com>
2023-10-28 17:46:55 -0300

    Class para calculadora

---
+++ calculadora.py
@@ -0,0 +1,35 @@
+
+class Calculadora:
+    
+    def __init__(self):
+        self.n1 = 0
+        self.n2 = 0
+
+    def somar(self):
+        return self.n1 + self.n2
+
+    def multiplicar(self):
+        return self.n1 * self.n2
+
+    def dividir(self):
+        if self.n2 == 0:
+            print("Não é possivel dividir por zero")
+            return
+        return self.n1 / self.n2
+
+    def elevar(self):
+        return self.n1 ** self.n2
+    
+    def subtractir(self):
+        return self.n1 - self.n2
+    
+    def verifica_numeros(self):
+        if not isinstance(self.n1, (int, float)):
+            print("O numero um não é valido")
+
+        if not isinstance(self.n2, (int, float)):
+            print("O numero dois não é valido")
+
+    def solicita_numeros(self):
+        self.n1 = float(input("Digite o primeiro numero: "))
+        self.n2 = float(input("Digite o segundo numero: "))
